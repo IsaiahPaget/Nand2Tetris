70f5176cb9e2a5967e3cfa1008c23b7e8b51bb75
Isaiah Paget <isaiah.paget@acceleratedfocus.com>
2023-10-07 00:12:15 -0500

    implemented multiplication in assembly

--- PartOne/04/mult/Mult.asm
+++ PartOne/04/mult/Mult.asm
@@ -9,4 +9,78 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// Psuedo Code
+
+// X = RAM[0]
+// Y = RAM[1]
+// Z = RAM[2]
+
+// for (i = 0 ; i < Y ; i++) {
+//      Z = Z + X;
+//}
+
+// operator = RAM[0]
+@R0
+D = M
+
+@operator
+M = D
+
+// operand = RAM[1]
+@R1
+D = M
+
+@operand
+M = D
+
+// Product = RAM[3]
+@sum
+M = 0
+
+// i = 0
+@i
+M = 1
+
+// For i in range of operator:
+//      Product = Product + operand
+(LOOP)
+
+    // get value of i
+    @i
+    D = M
+
+    // if i >= operator goto STOP
+    @operator
+    D = D - M
+    @STOP
+    D;JGT
+
+    // sum = sum + operand
+    @sum
+    D = M
+
+    @operand
+    D = D + M
+
+    @sum
+    M = D
+
+    // i = i + 1
+    @i
+    M = M + 1
+    @LOOP
+    0;JMP 
+
+
+// set RAM[2] = sum
+(STOP)
+
+    @sum
+    D = M
+    @R2
+    M=D
+
+
+(END)
+    @END
+    0;JMP
