dfd4023b573a0cd285ba436cfb7bac3427f30350
Isaiah Paget <isaiah.paget@acceleratedfocus.com>
2023-10-07 09:29:31 -0500

    made screen filll with black when key pressed

--- PartOne/04/fill/Fill.asm
+++ PartOne/04/fill/Fill.asm
@@ -12,3 +12,80 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//top left corner
+@point
+M = 0
+
+(LOOP)
+
+    // if keyboard is not pressed
+    @KBD
+    D = M
+    @BLANK
+    D;JEQ
+
+    @FILL
+    0;JMP
+
+(BLANK)
+
+    // d = point
+    @point
+    D = M
+
+    // if d > 0 goto LOOP
+    @LOOP
+    D;JLT
+
+    // d = point
+    @point
+    D = M
+
+    // calculate point
+    @SCREEN
+    A = A + D
+
+    // fill with white
+    M = 0
+
+    // point = point - 1
+    @point
+    M = M - 1
+
+    // goto LOOP
+    @LOOP
+    0;JMP
+
+(FILL)
+    
+    // d = point
+    @point
+    D = M
+
+    // max that point can be
+    @8192
+    D = D - A
+
+    // if d > 0 goto LOOP
+    @LOOP
+    D;JGE
+
+    // d = point
+    @point
+    D = M
+
+    // calculate point
+    @SCREEN
+    A = A + D
+
+    // fill black
+    M = -1
+
+    // point = point - 1
+    @point
+    M = M + 1
+
+    //goto FILL
+    @FILL
+    0;JMP
